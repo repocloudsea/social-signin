187f1625abd9742c618a1479b8ad3f9a82c6a8da
Shahbaz <jocnud@gmail.com>
2017-12-17 15:22:14 +0530

    enabled cros

--- src/main/java/com/shahbaz/cloudsecurity/CloudSecurityApplication.java
+++ src/main/java/com/shahbaz/cloudsecurity/CloudSecurityApplication.java
@@ -3,11 +3,9 @@ package com.shahbaz.cloudsecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-@CrossOrigin("*")
 public class CloudSecurityApplication {
 
 	public static void main(String[] args) {
